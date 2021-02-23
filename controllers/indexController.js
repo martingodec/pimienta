cf8e157a887cd34dba3d34fd6bc4300ec6c92ef0
Martin Godec <martingodec@hotmail.com>
2021-02-23 15:07:20 -0300

    Carga de nuevas funciones

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,8 +1,40 @@
 const indexController = {
 
+
     'index': (req, res) => {
 
-        res.render("index");
+
+        const menu = [
+            {
+    
+                "nombre": "Carpaccio fresco",
+                "descripcion": "Entrada Carpaccio de salmón con cítricos",
+                "precio": 65.50
+            },
+    
+            {
+    
+                "nombre": "Risotto de berenjena",
+                "descripcion": "Risotto de berenjena y queso de cabra",
+                "precio": 47.00
+            },
+    
+            {
+    
+                "nombre": "Mousse de arroz",
+                "descripcion": "Mousse de arroz con leche y aroma de azahar",
+                "precio": 27.50
+            },
+    
+            {
+    
+                "nombre": "Espárragos blancos",
+                "descripcion": "Espárragos blancos con vinagreta de verduras y jamón ibérico",
+                "precio": 37.50
+            }
+        ]
+
+        res.render("index", {"menu": menu});
 
     }
 
